a98d744ee605658d473bfa6873f9944e7eb16bf0
Natan Da Luz <159673834+NatanLuz@users.noreply.github.com>
2025-05-15 20:57:56 -0300

    Update escala_trab.py

--- projeto tatah/escala_trab.py
+++ projeto tatah/escala_trab.py
@@ -5,25 +5,26 @@ from datetime import datetime, timedelta
 def gerar_escala(nome_funcionario, data_inicial, semanas, folga_fixa='segunda'):
     dias_semana = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo']
     escala = []
-    data_atual = datetime.strptime(data_inicial, "%Y-%m-%d")  # Corrigido de 'data atual' para 'data_atual'
+    data_atual = datetime.strptime(data_inicial, "%Y-%m-%d")  
 
-    for semana in range(semanas):  # Corrigido de 'semana' para 'semanas'
+    for semana in range(semanas): 
         for dia in dias_semana:
             if dia == folga_fixa:
                 escala.append({'Data': data_atual.strftime("%Y-%m-%d"), 'Dia': dia, 'Status': 'Folga', 'Funcionário': nome_funcionario})
             elif dia == 'domingo':
-                # Adicionando a lógica para domingos: 2x1
+                
+                # Adicionando a lógica para domingos: 2 por 1
                 if (semana + 1) % 3 == 0:
                     escala.append({'Data': data_atual.strftime("%Y-%m-%d"), 'Dia': dia, 'Status': 'Folga', 'Funcionário': nome_funcionario})
                 else:
                     escala.append({'Data': data_atual.strftime("%Y-%m-%d"), 'Dia': dia, 'Status': 'Trabalho', 'Funcionário': nome_funcionario})
             else:
                 escala.append({'Data': data_atual.strftime("%Y-%m-%d"), 'Dia': dia, 'Status': 'Trabalho', 'Funcionário': nome_funcionario})
-            data_atual += timedelta(days=1)  # Corrigido a indentação
+            data_atual += timedelta(days=1)  # indentação
 
-    return escala  # Não estava retornando a escala
+    return escala  
+# Função dada para salvar a escala em Excel
 
-# Função para salvar a escala em Excel
 def salvar_escala_excel(escala, arquivo_excel):
     df = pd.DataFrame(escala)
     df.to_excel(arquivo_excel, index=False, engine='openpyxl')
@@ -35,5 +36,6 @@ data_inicial = "2024-10-01"  # Data inicial para gerar a escala
 semanas = 4  # Quantidade de semanas para gerar a escala
 
 # Gerar e salvar escala
-escala = gerar_escala(nome_funcionario, data_inicial, semanas)  # Corrigido o argumento
+
+escala = gerar_escala(nome_funcionario, data_inicial, semanas)  # argumento
 salvar_escala_excel(escala, "escala_trabalho.xlsx")
